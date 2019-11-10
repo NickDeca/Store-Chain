fafb51647cb060c93ad625c7b6dbd2f284e11399
nick deca <nikostgcube@gmail.com>
2019-11-10 20:07:35 +0200

    Add project files.

---
+++ Store_chain/Data/Store_chainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Store_chain.Models;
+
+namespace Store_chain.Data
+{
+    public class Store_chainContext : DbContext
+    {
+        public Store_chainContext (DbContextOptions<Store_chainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Store_chain.Models.Product> Product { get; set; }
+    }
+}
---
+++ Store_chain/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Store_chain.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+        public int category { get; set; }
+        public int department { get; set; }
+    }
+}
